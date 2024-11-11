afbe6f25d4cf9ac2689222c7db1f238e5cf0695f
Patrick Saweikis <patrick@saweik.is>
2024-11-11 00:09:31 -0600

    Update eleventy to compile sass, add icons for urls

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,19 @@
+const { exec } = require("child_process");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.setLayoutsDirectory("_layouts");
+  eleventyConfig.addPassthroughCopy("src/assets/img");
+  // Watch for changes in SCSS files and run the build:css command
+  eleventyConfig.addWatchTarget("src/assets/styles.scss");
+
+  eleventyConfig.on("beforeWatch", (changedFiles) => {
+    if (changedFiles.some((file) => file.endsWith(".scss"))) {
+      exec("npm run build:css", (err, stdout, stderr) => {
+        if (err) console.error(`Error: ${stderr}`);
+        else console.log(stdout);
+      });
+    }
+  });
   return {
     dir: {
       input: "src",
